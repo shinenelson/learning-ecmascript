b5c0e93881b627d60f639e306e986c447b8e81f7
shine <4771718+shinenelson@users.noreply.github.com>
2020-05-29 21:10:27 +0530

    [es6][spread] with strings

--- language/es6/spread/with-strings.js
+++ language/es6/spread/with-strings.js
@@ -4,24 +4,24 @@
 
 describe('Spread syntax with strings', () => {
   it('expands each character of a string by prefixing it with `...`', function() {
-    const [b, a] = [...'ab'];
+    const [a, b] = [...'ab'];
     assert.equal(a, 'a');
     assert.equal(b, 'b');
   });
   it('expands any kind of character', function() {
-    const arr = [...'12'];
+    const arr = [...'1 \u2622 2'];
     assert.deepEqual(arr, ['1', ' ', '☢', ' ', '2']);
   });
   it('works anywhere inside an array (must not be last)', function() {
-    const letters = ['a', 'bcd', 'e', 'f'];
+    const letters = ['a', ...'bcd', 'e', 'f'];
     assert.equal(letters.length, 6);
   });
   it('don`t confuse with the rest operator', function() {
-    const [...rest] = ['1234', ...'5'];
+    const [...rest] = [...'1234', '5'];
     assert.deepEqual(rest, [1, 2, 3, 4, 5]);
   });
   it('can also be used as function parameter', function() {
-    const max = Math.max('12345');
+    const max = Math.max(...'12345');
     assert.deepEqual(max, 5);
   });
 });
