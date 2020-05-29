5f2f51a62fff8e11bc6aba61ab13b70f9798abf8
shine <4771718+shinenelson@users.noreply.github.com>
2020-05-29 17:59:42 +0530

    [es6][block scoping] let

--- language/es6/block-scoping/let.js
+++ language/es6/block-scoping/let.js
@@ -6,13 +6,13 @@ describe('`let` restricts the scope of the variable to the current block', () =>
   describe('`let` vs. `var`', () => {
     it('`var` works as usual, it`s scope is the function', () => {
       if (true) {
-        let varX = true;
+        var varX = true;
       }
       assert.equal(varX, true);
     });
     it('`let` restricts scope to inside the block', () => {
       if (true) {
-        var letX = true;
+        let letX = true;
       }
       assert.throws(() => console.log(letX));
     });
@@ -21,12 +21,12 @@ describe('`let` restricts the scope of the variable to the current block', () =>
   describe('`let` usage', () => {
     it('`let` use in `for` loops', () => {
       let obj = {x: 1};
-      for (var key in obj) {}
+      for (let key in obj) {}
       assert.throws(() => console.log(key));
     });
     it('create artifical scope, using curly braces', () => {
       {
-        var letX = true;
+        let letX = true;
       }
       assert.throws(() => console.log(letX));
     });
