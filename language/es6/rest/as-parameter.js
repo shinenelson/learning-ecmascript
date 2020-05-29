1855e15a2687f125250411a2d43a86f8923cb4e2
shine <4771718+shinenelson@users.noreply.github.com>
2020-05-29 20:34:13 +0530

    [es6][rest] as parameter

--- language/es6/rest/as-parameter.js
+++ language/es6/rest/as-parameter.js
@@ -3,26 +3,26 @@
 
 describe('Rest parameters in functions', () => {
   it('must be the last parameter', () => {
-    const fn = (rest, veryLast) => {
+    const fn = (...rest) => {
       assert.deepEqual([1, 2], rest);
     };
     fn(1, 2);
   });
   it('can be used to get all other parameters', () => {
-    const fn = (firstParam, secondParam, rest) => {
+    const fn = (firstParam, secondParam, ...rest) => {
       assert.deepEqual([3,4], rest);
     };
     fn(null, 2, 3, 4);
   });
   it('makes `arguments` obsolete', () => {
-    const fn = () => {
+    const fn = (...args) => {
       assert.deepEqual([42, 'twenty three', 'win'], args);
     };
     fn(42, 'twenty three', 'win');
   });
   it('eliminate `arguments`!!!', () => {
     // using arguments here hangs test execution #TODO investigate
-    const fn = () => arguments;
+    const fn = (...arguments) => arguments;
     const [firstArg, ...rest] = fn(1, 2, 3);
     assert.deepEqual([2, 3], rest);
   });
