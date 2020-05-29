0111e43fb970c9dc99c7b720e2e11caa2420933a
shine <4771718+shinenelson@users.noreply.github.com>
2020-05-29 19:28:03 +0530

    [es6][object literal] computed properties
    note : could not fix one test - accessor keys can be computed names too

--- language/es6/object-literal/computed-properties.js
+++ language/es6/object-literal/computed-properties.js
@@ -5,24 +5,25 @@
 describe('Object literal properties may be computed values', () => {
   it('a computed property `x` needs to be surrounded by `[]`', () => {
     const propertyName = 'x';
-    const obj = {propertyName: 1};
+    const obj = {[propertyName]: 1};
     assert.equal(obj.x, 1);
   });
   it('can also get a function assigned', () => {
     const key = 'func';
-    const obj = {[key]: 'seven'};
+    const obj = {[key]: () => { return 'seven'; } };
     assert.equal(obj.func(), 'seven');
   });
   it('the key may also be the result of a function call', () => {
     const getName = () => 'propertyName';
-    const obj = {[getName]() {return 'seven'}};
+    const obj = {[getName()]: () => {return 'seven'}};
     assert.equal(obj.propertyName(), 'seven');
   });
   it('the key can also be constructed by an expression', () => {
-    const what = 'Key';
+    const what = 'tyName';
     const obj = {['proper' + what]: null};
     assert('propertyName' in obj);
   });
+  // can't solve this one
   it('accessor keys can be computed names too', () => {
     const obj = {
       set ['key'](_) {return 1},
