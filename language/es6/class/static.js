2cfca2a9a8598668a64d27ec672f38be6b779f92
shine <4771718+shinenelson@users.noreply.github.com>
2020-05-30 03:36:37 +0530

    [es6][class] static

--- language/es6/class/static.js
+++ language/es6/class/static.js
@@ -7,12 +7,12 @@ describe('Inside a class you can use the `static` keyword', () => {
     class UnitTest {}
     it('a static method just has the prefix `static`', () => {
       class TestFactory {
-        makeTest() { return new UnitTest(); }
+        static makeTest() { return new UnitTest(); }
       }
       assert.ok(TestFactory.makeTest() instanceof UnitTest);
     });
     it('the method name can be dynamic/computed at runtime', () => {
-      const methodName = 'makeTest';
+      const methodName = 'createTest';
       class TestFactory {
         static [methodName]() { return new UnitTest(); }
       }
@@ -22,14 +22,14 @@ describe('Inside a class you can use the `static` keyword', () => {
   describe('for accessors', () => {
     it('a getter name can be static, just prefix it with `static`', () => {
       class UnitTest {
-        get testType() { return 'unit'; }
+        static get testType() { return 'unit'; }
       }
       assert.equal(UnitTest.testType, 'unit');
     });
     it('even a static getter name can be dynamic/computed at runtime', () => {
       const type = 'test' + 'Type';
       class IntegrationTest {
-        get type() { return 'integration'; }
+        static get [type]() { return 'integration'; }
       }
       assert.ok('testType' in IntegrationTest);
       assert.equal(IntegrationTest.testType, 'integration');
