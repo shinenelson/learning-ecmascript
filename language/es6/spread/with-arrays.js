8e1d1da362ce075bdd9ae6f92c94c3e555359f11
shine <4771718+shinenelson@users.noreply.github.com>
2020-05-29 21:07:03 +0530

    [es6][spread] with arrays

--- language/es6/spread/with-arrays.js
+++ language/es6/spread/with-arrays.js
@@ -6,21 +6,21 @@ describe('Spread syntax with arrays', () => {
   describe('basically', () => {
     it('expands the items of an array by prefixing it with `...`', function() {
       const middle = [1, 2, 3];
-      const arr = [0, middle, 4];
+      const arr = [0, ...middle, 4];
       assert.deepEqual(arr, [0, 1, 2, 3, 4]);
     });
     it('an empty array expanded is no item', function() {
-      const arr = [0, [], 1];
+      const arr = [0, ...[], 1];
       assert.deepEqual(arr, [0, 1]);
     });
   });
   describe('is (in a way) the opposite to the rest syntax', function() {
     it('both use `...` to either expand all items and collect them', function() {
-      const [...rest] = [...[,1, 2, 3, 4, 5]];
+      const [...rest] = [...[1, 2, 3, 4, 5]];
       assert.deepEqual(rest, [1, 2, 3, 4, 5]);
     });
     it('rest syntax must be last in an array, spread can be used in any place', function() {
-      const [a, b, ...rest] = [1, [2, 3], 4, 5];
+      const [a, b, ...rest] = [1, 2, 3, 4, 5];
       assert.equal(a, 1);
       assert.equal(b, 2);
       assert.deepEqual(rest, [3, 4, 5]);
@@ -33,10 +33,10 @@ describe('Spread syntax with arrays', () => {
         assert.deepEqual(magicNumbers[0], magicA);
         assert.deepEqual(magicNumbers[1], magicB);
       };
-      fn(magicNumbers);
+      fn(...magicNumbers);
     });
     it('pass an array of numbers to Math.max()', function() {
-      const max = Math.max(...[23, 0, 42, 43]);
+      const max = Math.max(...[23, 0, 42, 41]);
       assert.equal(max, 42);
     });
   });  
@@ -45,7 +45,7 @@ describe('Spread syntax with arrays', () => {
       class X {
         constructor(a, b, c) { return [a, b, c]; }
       }
-      const args = [1];
+      const args = [1, 2, 3];
       assert.deepEqual(new X(...args), [1, 2, 3]);
     });
   });
