5819452fdb19e12a9475e63efa8702d1b5b6a879
shine <4771718+shinenelson@users.noreply.github.com>
2020-05-29 16:03:37 +0530

    [es6][template strings] basics

--- language/es6/template-strings/basics.js
+++ language/es6/template-strings/basics.js
@@ -5,7 +5,7 @@
 describe('A template string, is wrapped in ` (backticks) instead of \' or "', function() {
   describe('by default, behaves like a normal string', function() {
     it('just surrounded by backticks', function() {
-      var str = ``;
+      var str = `like a string`;
       assert.equal(str, 'like a string');
     });
   });
@@ -15,26 +15,26 @@ describe('A template string, is wrapped in ` (backticks) instead of \' or "', fu
   
   describe('can evaluate variables, which are wrapped in "${" and "}"', function() {
     it('e.g. a simple variable "${x}" just gets evaluated', function() {
-      var evaluated = `x=#x`;
+      var evaluated = `x=${x}`;
       assert.equal(evaluated, 'x=' + x);
     });
     it('multiple variables get evaluated too', function() {
-      var evaluated = '${ x } + $ { y }';
+      var evaluated = `${ x }+${ y }`;
       assert.equal(evaluated, x + '+' + y);
     });
   });
 
   describe('can evaluate any expression, wrapped inside "${...}"', function() {
     it('all inside "${...}" gets evaluated', function() {
-      var evaluated = `${ x } + ${ y }`;
+      var evaluated = `${ x + y }`;
       assert.equal(evaluated, x+y);
     });
     it('inside "${...}" can also be a function call', function() {
-      function getDomain(){ 
-        return document.domain; 
+      function getEnv(){
+        return 'ECMAScript 6';
       }
-      var evaluated = `${ getDomain }`;
-      assert.equal(evaluated, 'tddbin.com');
+      var evaluated = `${ getEnv() }`;
+      assert.equal(evaluated, 'ECMAScript 6');
     });
   });
 });
