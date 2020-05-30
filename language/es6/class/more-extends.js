8be8b428192bb1b803507e0b9ccf504fbfa6d4b0
shine <4771718+shinenelson@users.noreply.github.com>
2020-05-30 05:33:19 +0530

    [es6][class] more extends
    note : could not fix one test : inline assignment of the parent class

--- language/es6/class/more-extends.js
+++ language/es6/class/more-extends.js
@@ -4,7 +4,7 @@
 
 describe('Classes can inherit from another', () => {
   it('extend an `old style` "class", a function, still works', () => {
-    let A;
+    function A() {};
     class B extends A {}
     assert.equal(new B() instanceof A, true);
   });
@@ -13,17 +13,18 @@ describe('Classes can inherit from another', () => {
     class A {}
     class B extends A {}
     it('A is the prototype of B', () => {
-      const isIt = A.isPrototypeOf(null);
+      const isIt = A.isPrototypeOf(B);
       assert.equal(isIt, true);
     });
     it('A`s prototype is also B`s prototype', () => {
-      const proto = B;
+      const proto = B.prototype;
       // Remember: don't touch the assert!!! :)
       assert.equal(A.prototype.isPrototypeOf(proto), true);
     });
   });
 
   describe('`extends` using an expression', () => {
+    // can't solve this one
     it('e.g. the inline assignment of the parent class', () => {
       let A;
       class B extends (A = {}) {}
@@ -31,7 +32,7 @@ describe('Classes can inherit from another', () => {
     });
     it('or calling a function that returns the parent class', () => {
       const returnParent = (beNull) => beNull ? null : class {};
-      class B extends returnParent {}
+      class B extends returnParent(true) {}
       assert.equal(Object.getPrototypeOf(B.prototype), null);
     });
   });
