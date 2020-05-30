9a2cf039bb43402cc5933c6259d79c75930d8208
shine <4771718+shinenelson@users.noreply.github.com>
2020-05-30 06:57:10 +0530

    [es6][array API] findIndex

--- language/es6/array-api/findIndex.js
+++ language/es6/array-api/findIndex.js
@@ -4,21 +4,26 @@
 
 describe('`Array.prototype.findIndex` makes finding items in arrays easier', () => {
   it('takes a compare function, returns the index where it returned true', function() {
-    const foundAt = [false, true].findIndex(item);
+    const func = (item, index) => {
+      if ( item === true ) {
+       return index
+      };
+    }
+    const foundAt = [false, true].findIndex(func);
     assert.equal(foundAt, 1);
   });
   it('returns the first position it was found at', function() {
-    const foundAt = [0, 1, 1, 1].findIndex(item => item = 1);
+    const foundAt = [0, 1, 1, 1].findIndex(item => item === 1);
     assert.equal(foundAt, 1);
   });
   it('returns `-1` when nothing was found', function() {
-    const foundAt = [1, 2, 3].findIndex(item => item > 1);
+    const foundAt = [1, 2, 3].findIndex(item => item < 1);
     assert.equal(foundAt, -1);
   });
   it('the findIndex callback gets the item, index and array as arguments', function() {
     const three = 3;
     const containsThree = arr => arr.indexOf(three) > -1;
-    function theSecondThree(index, arr) {
+    function theSecondThree(item, index, arr) {
       const preceedingItems = arr.slice(0, index);
       return containsThree(preceedingItems);
     }
@@ -28,7 +33,7 @@ describe('`Array.prototype.findIndex` makes finding items in arrays easier', () 
   it('combined with destructuring complex compares become short', function() {
     const bob = {name: 'Bob'};
     const alice = {name: 'Alice'};
-    const foundAt = [bob, alice].findIndex(({name:{length:l}}) => length > 3);
+    const foundAt = [bob, alice].findIndex(({name:{length}}) => length > 3);
     assert.equal(foundAt, 1);
   });
 });
