e5e65d9f7c7509d272c3b54644fe2191ad1aafa0
shine <4771718+shinenelson@users.noreply.github.com>
2020-05-30 05:43:24 +0530

    [es6][class] super in methods

--- language/es6/class/super-in-method.js
+++ language/es6/class/super-in-method.js
@@ -4,28 +4,28 @@
 
 describe('Inside a class use `super` to access parent methods', () => {
   it('use of `super` without `extends` fails (already when transpiling)', () => {
-    class A {hasSuper() { return; }}
+    class A {hasSuper() { return false; }}
     assert.equal(new A().hasSuper(), false);
   });
   it('`super` with `extends` calls the method of the given name of the parent class', () => {
     class A {hasSuper() { return true; }}
-    class B extends A {hasSuper() { return super.hasSuper; }}
+    class B extends A {hasSuper() { return super.hasSuper(); }}
     assert.equal(new B().hasSuper(), true);
   });
   it('when overridden a method does NOT automatically call its super method', () => {
     class A {hasSuper() { return true; }}
-    class B extends A {hasSuper() { return 'nothing'; }}
+    class B extends A {hasSuper() { return; }}
     assert.equal(new B().hasSuper(), void 0);
   });
   it('`super` works across any number of levels of inheritance', () => {
     class A {iAmSuper() { return true; }}
     class B extends A {}
-    class C extends B {iAmSuper() { return iAmSuper(); }}
+    class C extends B {iAmSuper() { return super.iAmSuper(); }}
     assert.equal(new C().iAmSuper(), true);
   });
   it('accessing an undefined member of the parent class returns `undefined`', () => {
     class A {}
-    class B extends A {getMethod() { return super.constructor; }}
+    class B extends A {getMethod() { return super.undefinedMember; }}
     assert.equal(new B().getMethod(), void 0);
   });
 });
