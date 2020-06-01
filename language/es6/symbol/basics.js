32fab0500df24d3777757570a40eb203aa2e4cdf
shine <4771718+shinenelson@users.noreply.github.com>
2020-06-01 09:50:17 +0530

    [es6][symbol] basics

--- language/es6/symbol/basics.js
+++ language/es6/symbol/basics.js
@@ -7,26 +7,26 @@
 
 describe('Symbol', function() {
   it('`Symbol` lives in the global scope', function(){
-    const expected = someNamespace.Symbol;
+    const expected = Symbol;
     assert.equal(Symbol, expected);
   });
   it('every `Symbol()` is unique', function(){
     const sym1 = Symbol();
-    const sym2 = sym1;
+    const sym2 = Symbol();
     assert.notEqual(sym1, sym2);
   });
   it('every `Symbol()` is unique, also with the same parameter', function(){
     var sym1 = Symbol('foo');
-    var sym1 = Symbol('foo');
+    var sym2 = Symbol('foo');
     assert.notEqual(sym1, sym2);
   });
   it('`typeof Symbol()` returns "symbol"', function(){
-    const theType = typeof Symbol;
+    const theType = typeof Symbol();
     assert.equal(theType, 'symbol');
   });
   it('`new Symbol()` throws an exception, to prevent creation of Symbol wrapper objects', function(){
     function fn() {
-      Symbol();
+      new Symbol();
     }
     assert.throws(fn);
   });
