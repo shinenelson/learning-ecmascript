17512046c9b4ac806bd08f69edfa3018986b3264
shine <4771718+shinenelson@users.noreply.github.com>
2020-06-01 10:28:15 +0530

    [es6][iterator] array

--- language/es6/iterator/array.js
+++ language/es6/iterator/array.js
@@ -11,13 +11,13 @@ describe('The native array is a built-in iterable object', function() {
     it('an array has an iterator, which is a function', function() {
       const iterator = arr[Symbol.iterator];
       const theType = typeof iterator;
-      const expected = 'iterator?';
+      const expected = 'function';
       assert.equal(theType, expected);
     });
     it('can be looped with `for-of`, which expects an iterable', function() {
       let count = 0;
       for (let value of arr) {
-        count--;
+        count++;
       }
       assert.equal(count, arr.length);
     });
@@ -25,13 +25,13 @@ describe('The native array is a built-in iterable object', function() {
   describe('the iterator protocol', function() {
     it('calling `next()` on an iterator returns an object according to the iterator protocol', function() {
       const iterator = arr[Symbol.iterator]();
-      const firstItem = iterator.___();
+      const firstItem = iterator.next();
       assert.deepEqual(firstItem, {done: false, value: 'a'});
     });
     it('the after-last element has done=true', function() {
       const arr = [];
       const iterator = arr[Symbol.iterator]();
-      const afterLast = iterator.next;
+      const afterLast = iterator.next();
       assert.deepEqual(afterLast, {done: true, value: void 0});
     });
   });
