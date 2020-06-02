5a66532483c0d53ecbd56790fbdca5a5298b95f4
shine <4771718+shinenelson@users.noreply.github.com>
2020-06-02 14:29:07 +0530

    [es6][default parameters] basics

--- language/es6/default-parameters/basics.js
+++ language/es6/default-parameters/basics.js
@@ -4,29 +4,29 @@
 
 describe('Default parameters make function parameters more flexible', () => {
   it('define it using an assignment to the parameter `function(param=1){}`', function() {
-    let aNumber = (int) => int;
+    let aNumber = (int = 0) => int;
     assert.equal(aNumber(), 0);
   });
   it('it is used when `undefined` is passed', function() {
     let aNumber = (int = 23) => int;
-    const param = 42;
+    let param;
     assert.equal(aNumber(param), 23);
   });
   it('it is not used when a value is given', function() {
-    function xhr() {
+    function xhr(method = 'GET') {
       return method;  
     }
     assert.equal(xhr('POST'), 'POST');
   });
   it('it is evaluated at run time', function() {
-    let defaultValue;
+    let defaultValue = 42;
     function xhr(method = `value: ${defaultValue}`) {
       return method;  
     }
     assert.equal(xhr(), 'value: 42');
   });
   it('it can also be a function', function() {
-    const defaultValue = 0;
+    function defaultValue() { return 'defaultValue' };
     function fn(value = defaultValue()) {
       return value;  
     }
