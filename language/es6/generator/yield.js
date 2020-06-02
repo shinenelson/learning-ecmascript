a813c92a85352ca5a2f3c00a7636a628d5195cd8
shine <4771718+shinenelson@users.noreply.github.com>
2020-06-02 11:21:52 +0530

    [es6][generator] yield

--- language/es6/generator/yield.js
+++ language/es6/generator/yield.js
@@ -13,23 +13,23 @@ describe('Generator - `yield` is used to pause and resume a generator function',
     generator = generatorFunction();
   });
   it('converting a generator to an array (using `Array.from`) resumes the generator until all values are received', () => {
-    let values = Array.from(generatorFunction);
+    let values = Array.from(generator);
     assert.deepEqual(values, ['hello', 'world']);
   });
   describe('after the first `generator.next()` call', function() {
     it('the value is "hello"', function() {
-      const {value} = generator.next;
+      const {value} = generator.next();
       assert.equal(value, 'hello');
     });
     it('and `done` is false', function() {
-      const {done} = generator;
+      const {done} = generator.next();
       assert.equal(done, false);
     });
   });
   describe('after the second `next()` call', function() {
     let secondItem;
     beforeEach(function() {
-      
+      generator.next()
       secondItem = generator.next();
     });
     it('`value` is "world"', function() {
@@ -37,7 +37,7 @@ describe('Generator - `yield` is used to pause and resume a generator function',
       assert.equal(value, 'world');
     });
     it('and `done` is still false', function() {
-      const done = secondItem;
+      const {done} = secondItem;
       assert.equal(done, false);
     });
   });
@@ -45,7 +45,7 @@ describe('Generator - `yield` is used to pause and resume a generator function',
     it('`done` property equals true, since there is nothing more to iterator over', function() {
       generator.next();
       generator.next();
-      let done = generator.done;
+      let {done} = generator.next();
       assert.equal(done, true);
     });
   });
