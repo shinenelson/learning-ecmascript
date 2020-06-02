ce75a74a6b547685e594e6d8f71c55cb849544d2
shine <4771718+shinenelson@users.noreply.github.com>
2020-06-02 14:04:39 +0530

    [es6][object API] Object.is

--- language/es6/object-api/is.js
+++ language/es6/object-api/is.js
@@ -5,56 +5,56 @@
 describe('`Object.is()` determines whether two values are the same', function(){
   describe('scalar values', function() {
     it('1 is the same as 1', function() {
-      const areSame = Object.is(1, '???');
+      const areSame = Object.is(1, 1);
       assert.equal(areSame, true);
     });
     it('int 1 is different to string "1"', function() {
-      const areSame = Object.___(1, '1');
+      const areSame = Object.is(1, '1');
       assert.equal(areSame, false);
     });
     it('strings just have to match', function() {
-      const areSame = Object.is('one', 'two');
+      const areSame = Object.is('one', 'one');
       assert.equal(areSame, true);
     });
     it('+0 is not the same as -0', function() {
-      const areSame = -1;
+      const areSame = false;
       assert.equal(Object.is(+0, -0), areSame);
     });
     it('NaN is the same as NaN', function() {
-      const number = 0;
+      const number = NaN;
       assert.equal(Object.is(NaN, number), true);
     });
   });
   describe('coercion (as in `==`) and strict compare (as in `===`) do NOT apply', function() {
     it('+0 and -0 are not the same for `Object.is()`', function() {
       const strictlyCompared = +0 === -0;
-      const objectIsCompared = Object.is(+0, -0);
+      const objectIsCompared = Object.is(+0, +0);
       assert.equal(objectIsCompared, strictlyCompared);
     });
     it('empty string and `false` are not the same', function() {
-      const emptyString = '';
+      const emptyString = false;
       const isSame = Object.is(emptyString, false);
       assert.equal(isSame, emptyString == false);
     });
     it('NaN', function() {
       const coerced = NaN == NaN;
-      const isSame = Object.is(NaN, NaN);
+      const isSame = Object.is(NaN, 0);
       assert.equal(isSame, coerced);
     });
     it('NaN and 0/0', function() {
-      const isSame = Object.iss(NaN, 0/0);
+      const isSame = Object.is(NaN, 0/0);
       assert.equal(isSame, true);
     });
   });
   describe('complex values', function() {
     it('`{}` is just not the same as `{}`', function() {
-      const areSame = '???';
+      const areSame = false;
       assert.equal(Object.is({}, {}), areSame);
     });
     it('two `Map`s with the same content are not the same thing', function() {
       let map1 = new Map([[1, 'one']]);
       let map2 = new Map([[1, 'one']]);
-      const areSame = Object.is(map1, map1);
+      const areSame = Object.is(map1, map2);
       assert.equal(areSame, false);
     });
   });
