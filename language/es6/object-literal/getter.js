45a8e4bb52e4cff625ce097597d16b78211b661f
shine <4771718+shinenelson@users.noreply.github.com>
2020-06-03 11:05:41 +0530

    [es6][object literal] getter

--- language/es6/object-literal/getter.js
+++ language/es6/object-literal/getter.js
@@ -5,20 +5,20 @@
 describe('An object literal can also contain getters', () => {
   it('just prefix the property with `get` (and make it a function)', function() {
     const obj = {
-      x() { return 'ax'; }
+      get x() { return 'ax'; }
     };
     assert.equal(obj.x, 'ax');
   });
   it('must have NO parameters', function() {
     const obj = {
-      x(param) { return 'ax'; }
+      get x() { return 'ax'; }
     };
     assert.equal(obj.x, 'ax');
   });
   it('can be a computed property (an expression enclosed in `[]`)', function() {
     const keyName = 'x';
     const obj = {
-      get keyName() { return 'ax'; }
+      get [keyName]() { return 'ax'; }
     };
     assert.equal(obj.x, 'ax');
   });
@@ -26,7 +26,7 @@ describe('An object literal can also contain getters', () => {
     const obj = {
       get x() { return 'ax'; }
     };
-    delete obj.y;
+    delete obj.x;
     assert.equal(obj.x, void 0);
   });
 
