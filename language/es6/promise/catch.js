909587992af59a9adb9bcd0fab82c9f6d1739985
shine <4771718+shinenelson@users.noreply.github.com>
2020-06-03 14:22:06 +0530

    [es6][promise] catch

--- language/es6/promise/catch.js
+++ language/es6/promise/catch.js
@@ -7,26 +7,26 @@
 describe('`catch()` returns a Promise and deals with rejected cases only', () => {
   describe('prerequisites for understanding', () => {
     it('*return* a fulfilled promise, to pass a test', () => {
-      Promise.resolve();
+      return Promise.resolve();
       assert(false); // Don't touch! Make the test pass in the line above!
     });
     it('reminder: the test passes when a fulfilled promise is returned', () => {
-      return Promise.reject('I should fulfill.');
+      return Promise.resolve('I should fulfill.');
     });
   });
   describe('`catch` method basics', () => {
     it('is an instance method', () => {
-      const p = Promise;
+      const p = Promise.resolve();
       assert.equal(typeof p.catch, 'function');
     });
     it('catches only promise rejections', (done) => {
-      const promise = Promise.resolve();
+      const promise = Promise.reject();
       promise
         .then(() => { done('Should not be called!'); })
         .catch(done);
     });
     it('returns a new promise', () => {
-      const whatToReturn = () => Promise.reject();
+      const whatToReturn = () => Promise.resolve();
       const promise = Promise.reject();
       return promise.catch(() =>
         whatToReturn()
@@ -34,11 +34,11 @@ describe('`catch()` returns a Promise and deals with rejected cases only', () =>
     });
     it('converts it`s return value into a promise', () => {
       const p = Promise.reject();
-      const p1 = p.catch(() => void 0);
+      const p1 = p.catch(() => 'promise?');
       return p1.then(result => assert.equal('promise?', result));
     });
     it('the first parameter is the rejection reason', () => {
-      const p = Promise.reject('rejection');
+      const p = Promise.reject('oops');
       return p.catch(reason => {
         assert.equal(reason, 'oops');
       });
@@ -46,7 +46,7 @@ describe('`catch()` returns a Promise and deals with rejected cases only', () =>
   });
   describe('multiple `catch`es', () => {
     it('only the first `catch` is called', () => {
-      const p = Promise.reject('1');
+      const p = Promise.resolve('1 AND 2');
       const p1 = p
           .catch(reason => void 0)
           .catch(reason => `${reason} AND 3`)
@@ -56,7 +56,7 @@ describe('`catch()` returns a Promise and deals with rejected cases only', () =>
       );
     });
     it('if a `catch` throws, the next `catch` catches it', () => {
-      const p = Promise.reject('1');
+      const p = Promise.resolve('1 AND 2 AND 3');
       const p1 = p
           .catch(reason => { throw Error(`${reason} AND 2`) })
           .catch(err => { throw Error(`${err.message} AND 3`) })
