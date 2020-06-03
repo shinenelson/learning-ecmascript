a8c013fef2e566731c1ee49aaee0e872bf61d83a
shine <4771718+shinenelson@users.noreply.github.com>
2020-06-03 13:03:18 +0530

    [es6][number API] parseInt

--- language/es6/number-api/parseInt.js
+++ language/es6/number-api/parseInt.js
@@ -4,15 +4,15 @@
 
 describe('`Number.parseInt()` parses a string and returns an integer.', () => {
   it('it is a static function on `Number`', () => {
-    const whatType = 'global';
+    const whatType = 'function';
     assert.equal(typeof Number.parseInt, whatType);
   });
   it('is the same as the global function `parseInt`', () => {
-    const parseIntFunction = Number.parse_int;
+    const parseIntFunction = Number.parseInt;
     assert.equal(parseIntFunction, global().parseInt);
   });
   it('`parseInt` was specified in ECMAScript v1 (please find that ES1 kata for learning more)', () => {
-    const version = 6;
+    const version = 1;
     assert.equal('ECMAScript v' + version, 'ECMAScript v1');
   });
 });
