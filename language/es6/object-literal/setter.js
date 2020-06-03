0018ee273b066ae0c0d42baff9d17b7e6d8794c3
shine <4771718+shinenelson@users.noreply.github.com>
2020-06-03 11:19:17 +0530

    [es6][object literal] setter

--- language/es6/object-literal/setter.js
+++ language/es6/object-literal/setter.js
@@ -7,7 +7,7 @@ describe('An object literal can also contain setters', () => {
     it('by prefixing the property with `set` (and make it a function)', function() {
       let theX = null;
       const obj = {
-        x(newX) { theX = newX; }
+        set x(newX) { theX = newX; }
       };
       obj.x = 'the new X';
       assert.equal(theX, 'the new X');
@@ -15,7 +15,7 @@ describe('An object literal can also contain setters', () => {
     it('must have exactly one parameter', function() {
       let setterCalledWith = void 0;
       const obj = {
-        x() { // <<<<=== it's not a setter yet!
+        set x(arg) { // <<<<=== it's not a setter yet!
           if (arguments.length === 1) {
             setterCalledWith = arguments[0];
           }
@@ -28,8 +28,9 @@ describe('An object literal can also contain setters', () => {
       const privatePropertyName = '_' + publicPropertyName;
       const obj = {
         [privatePropertyName]: null,
-        
-        // write the complete setter to make the assert below pass :)
+        set x(arg) {
+          this._x = arg;
+        }
       };
       obj.x = 'axe';
       assert.equal(obj._x, 'axe');
@@ -41,8 +42,7 @@ describe('An object literal can also contain setters', () => {
       const obj = {
         set x(param) { setterCalled = true; }
       };
-      // delete the property x here, to make the test pass
-      
+      delete obj.x;
       obj.x = true;
       assert.equal(setterCalled, false);
     });
